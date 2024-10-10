5778225126881cff6efc0c44556b6e5a228288de
mercy <valve.ubisoft@gmail.com>
2024-10-10 17:51:40 +0300

    Add AppData static path

--- save manager/StaticPath.cs
+++ save manager/StaticPath.cs
@@ -2,11 +2,12 @@
 {
     internal class StaticPath
     {
+        public static string AppDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
         public static string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/backup";
         public static string DarkSoulsRemasteredPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/NBGI/DARK SOULS REMASTERED";
-        public static string DarkSoulsIIPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/DarkSoulsII";
-        public static string DarkSoulsIIIPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/DarkSoulsIII";
-        public static string SekiroPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/Sekiro";
-        public static string EldenRingPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/EldenRing";
+        public static string DarkSoulsIIPath = AppDataPath + "/DarkSoulsII";
+        public static string DarkSoulsIIIPath = AppDataPath + "/DarkSoulsIII";
+        public static string SekiroPath = AppDataPath + "/Sekiro";
+        public static string EldenRingPath = AppDataPath + "/EldenRing";
     }
 }
